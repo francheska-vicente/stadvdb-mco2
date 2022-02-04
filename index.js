bf9ef5b52b3c4810890ee8f76ebfce2cff287c32
Francheska Vicente <francheska_vicente@dlsu.edu.ph>
2022-02-04 16:25:30 +0800

    Update index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+//import the necessary modules
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const routes = require('./routes/routes.js');
+const db = require('./models/db.js');
+const app = express();
+
+//parse incoming requests with urlencoded payloads
+app.use(express.urlencoded({ extended: false }));
+//parse incoming json payload
+app.use(express.json());
+
+//bind the server to a port and a host
+app.listen(process.env.PORT, process.env.HOSTNAME, function () {
+    console.log(
+        `Server is running at http://${process.env.HOSTNAME}:${process.env.PORT}`
+    );
+});
+
+module.exports = app;
