fc75e0cbfb4f52828a6c594ca843dd369624c1f8
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2022-02-10 05:34:52 +0800

    Added query string builder function

---
+++ helpers/queryHelper.js
@@ -0,0 +1,67 @@
+const query_funcs = {
+    to_select_query: function (movies) {
+        var query = `SELECT `;
+
+        for (var i = 0; i < select.length; i++) {
+            if (i != 0 && select[i].split(" ").join("") != `rank`) {
+                query = query + `, ` + select[i];
+            } else if (i != 0) {
+                query = query + `, \`` + select[i] + `\``;
+            } else {
+                query = query + select[i];
+            }
+        }
+
+        if (select.length == 0) {
+            query = query + '*';
+        }
+
+        query = query + ` FROM movies `;
+
+        for (var i = 0; i < where.length; i++) {
+            if (i != 0) {
+                query = query + `, ` + where[i];
+            } else {
+                query = query + ` WHERE ` + where[i];
+            }
+        }
+
+        return query + `;`;
+    },
+
+    to_insert_query: function (movies) {
+        var name = movies.name;
+        var rank = (movies.rank) ? movies.rank : `NULL`;
+        var year = movies.year;
+
+        return `INSERT INTO movies (name, \`rank\`,  year) VALUES ('` + name + `', ` + rank + `, ` + year + `);`
+    },
+
+    to_update_query: function (id, name, rank, year) {
+        var query = `UPDATE movies SET`;
+
+        if (name != '') {
+            query = query + ` name = '` + name + `'`;
+
+            if (rank != '') {
+                query = query + `, \`rank\` = ` + rank;
+
+                if (year != '') {
+                    query = query + `, year = ` + year;
+                }
+            } else if (year != '') {
+                query = query + `, year = ` + year;
+            }
+        } else {
+            query = query + ` \`rank\` = ` + rank;
+            query = query + `year = ` + year;
+        }
+
+        return query + ` WHERE id = ` + id + `;`;
+    },
+
+    to_delete_query: function (id) {
+        return `DELETE FROM movies WHERE id = ` + id + `;`;
+    },
+}
+module.exports = query_funcs;
