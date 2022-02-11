536d4011082d0b1f5a99443bc4fec1689ee66b8c
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2022-02-11 11:59:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const express = require('express');
 const hbs = require('hbs');
 const routes = require('./routes/routes.js');
 const db = require('./models/db.js');
+const repl = require('./models/replicator.js');
 const app = express();
 
 //parse incoming requests with urlencoded payloads
@@ -19,15 +20,8 @@ var hostname = process.env.HOSTNAME;
 /* The page should only be accessible once the database is connected. */
 app.use('/', routes);
 app.listen(process.env.PORT, process.env.HOSTNAME, function () {
-    console.log(
-        `Server is running at http://${hostname}:${port}`
-    );
-    //db.connect(1, function (result) { console.log(result) });
-    // db.execute_query(`SELECT * FROM movies`).then(data => {
-    //     console.log(data);
-    // });
-
-    db.insert_query({ name: `Pia was here hehe`, year: 1979, rank: 4.0 }).then(data => {
+    console.log(`Server is running at http://${hostname}:${port}`);
+    db.insert_query(`Pia was here hehe`, 1979, 4.0).then(data => {
         console.log(data);
     });
 });
