3369b187f1d2e86ad426f75a86df09cdc2e439b1
Francheska Vicente <80678453+francheska-vicente@users.noreply.github.com>
2022-02-12 23:21:45 +0800

    Added search bar functionality
    Error message does not show yet on the home page, but the search bar works.

--- controllers/controller.js
+++ controllers/controller.js
@@ -9,7 +9,7 @@ const controller = {
                         return acc.concat(Object.keys(obj).filter(key => acc.indexOf(key) === -1));
                 }, []);  
                 
-                result.sort((a, b) => a.id - b.id);
+                result = result.sort((a, b) => a.id - b.id);
                 resultlen = result.length;
                 result = result.slice(0, 50);
                 data = {
@@ -19,6 +19,34 @@ const controller = {
                 }      
                 console.log('hello' + data);
                 res.render ('home', data);
+        },
+
+        getQueryResults: async function (req, res) {
+                var result = [];
+                var query = req.query.query_holder;
+
+                if (query.trim().split(" ")[0].toUpperCase () == 'SELECT') {
+                        result = await db.select_query(query);
+
+                        var uniqueKeys = result.reduce(function (acc, obj) {
+                                return acc.concat(Object.keys(obj).filter(key => acc.indexOf(key) === -1));
+                        }, []);  
+                        
+                        resultlen = result.length;
+                        result = result.slice(0, 50);
+                        
+                        var data = {
+                                uniqueKeys: uniqueKeys,
+                                result: result,
+                                resultlen: resultlen
+                        };
+
+                        res.render('home', data);
+                } else {
+                        console.log ('Query not allowed.');
+                        var error = "Only SELECT queries can be executed."
+                        res.render('home', error);
+                }
         }
 }
 
--- routes/routes.js
+++ routes/routes.js
@@ -3,5 +3,7 @@ const controller = require('../controllers/controller.js');
 const app = express.Router();
 
 app.get('/', controller.getIndex);
+// app.get('/:pageNumber', controller.getPage);
+app.get('/query-search/results', controller.getQueryResults);
 
 module.exports = app;
