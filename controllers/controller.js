c4938e36bb0a29f8ef84a22f5490f115200e1f2e
Francheska Vicente <80678453+francheska-vicente@users.noreply.github.com>
2022-02-12 19:43:50 +0800

    Update controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,19 +1,22 @@
 const db = require ('../models/db.js');
 const controller = {
     getIndex: async function (req, res) {
-        console.log('hello');
         var result = [];
-        result = await db.select_query('SELECT id,`rank`,name FROM movies;');
+        var query = `SELECT * FROM movies;`
+        result = await db.select_query(query);
+        
         var uniqueKeys = result.reduce(function (acc, obj) {
             return acc.concat(Object.keys(obj).filter(key => acc.indexOf(key) === -1));
         }, []);  
+        
         result.sort((a, b) => a.id - b.id);
-        result = result.slice(0, 3);
-        data = {
+        result = result.slice(0, 50);
+        
+        var data = {
             uniqueKeys: uniqueKeys,
             result: result
-        }      
-        console.log('hello' + data);
+        };
+        
         res.render ('home', data);
     }
 }
