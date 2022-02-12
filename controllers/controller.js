4a7d98e1672f479df48583d4445447851046a5f3
ajmarcelo <andreajean1014@gmail.com>
2022-02-12 18:10:27 +0800

    Fixed fill up table

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,20 @@
+const db = require ('../models/db.js');
 const controller = {
-    getIndex: function (req, res) {
-        res.render('home');
+    getIndex: async function (req, res) {
+        console.log('hello');
+        var result = [];
+        result = await db.select_query('SELECT id,`rank`,name FROM movies;');
+        var uniqueKeys = result.reduce(function (acc, obj) {
+            return acc.concat(Object.keys(obj).filter(key => acc.indexOf(key) === -1));
+        }, []);  
+        result.sort((a, b) => a.id - b.id);
+        result = result.slice(0, 3);
+        data = {
+            uniqueKeys: uniqueKeys,
+            result: result
+        }      
+        console.log('hello' + data);
+        res.render ('home', data);
     }
 }
 
--- public/js/home.js
+++ public/js/home.js
@@ -1,23 +1,8 @@
+//const { DateTime } = require("luxon");
+
 $(document).ready(function () {
-    // Get the text for the Handlebars template from the script element.
-    var templateText = $("#tableTemplate").html();
-
-    // Compile the Handlebars template.
-    var tableTemplate = Handlebars.compile(templateText);
-
-    // Define an array of people.
-    var people = [
-        { "Id": 1, "First Name": "Anthony", "Last Name": "Nelson", "Age": 25 },
-        { "Id": 2, "First Name": "Helen", "Last Name": "Garcia", "Age": 32 },
-        { "Id": 3, "First Name": "John", "Last Name": "Williams", "Age": 48 }
-    ];
-    
-    // Evaluate the template with an array of people and set the HTML
-    // for the people table.
-    $("#movies-table").html(tableTemplate({ array: people }));
-
-    // enableSearch();
-    // searchQuery();
+    enableSearch();
+    // submitQuery();
 });
 
 function enableSearch() { //on change
@@ -29,11 +14,9 @@ function enableSearch() { //on change
     });      
 }
 
-function searchQuery() {
+function submitQuery() {
     $('form').on('submit', function (event) {
         let query = $('#query').val().trim();
         
     });
 }
-
-
