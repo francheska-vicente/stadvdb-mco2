94981482a0d690e0a399f90530db4a6c518f7420
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2022-02-13 18:45:16 +0800

    Update transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -5,6 +5,47 @@ const nodes = require('../models/nodes.js');
 const queryHelper = require('../helpers/queryHelper.js');
 
 const transactions_funcs = {
+    make_transaction_with_log2: async function (node, query, log, log2, type, id) {
+        try {
+            let conn = await nodes.connect_node(node);
+            if (conn)
+                try {
+                    await conn.beginTransaction();
+
+                    if (type === 'UPDATE' || type === 'DELETE')
+                        await nodes.execute_query(conn, queryHelper.to_select_for_update(id));
+
+                    await nodes.execute_query(conn, `SET @@session.time_zone = "+08:00";`);
+                    var result = await nodes.execute_query(conn, query);
+                    console.log('Executed query!');
+
+                    var log = await nodes.execute_query(conn, log);
+                    console.log('Created log!');
+
+                    var log = await nodes.execute_query(conn, log2);
+                    console.log('Created log 2!');
+
+                    await conn.commit();
+                    await conn.release();
+                    return result;
+                }
+                catch (error) {
+                    console.log(error)
+                    console.log('Rolled back the data.');
+                    conn.rollback(node);
+                    return error;
+                }
+            else {
+                console.log('Unable to connect!');
+            }
+        }
+        catch (error) {
+            console.log(error);
+            console.log('Unable to connect!');
+            return error;
+        }
+    },
+
     make_transaction_with_log: async function (node, query, log, type, id) {
         try {
             let conn = await nodes.connect_node(node);
@@ -18,10 +59,10 @@ const transactions_funcs = {
                     await nodes.execute_query(conn, `SET @@session.time_zone = "+08:00";`);
                     var result = await nodes.execute_query(conn, query);
                     console.log('Executed query!');
-                    
+
                     var log = await nodes.execute_query(conn, log);
                     console.log('Created log!');
-                    
+
                     await conn.commit();
                     await conn.release();
                     return result;
