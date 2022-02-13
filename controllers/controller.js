0fa3b3b8831657f5a7f417536e41a15bc219fa2f
Francheska Vicente <francheska_vicente@dlsu.edu.ph>
2022-02-13 11:34:35 +0800

    Added id to select queries

--- controllers/controller.js
+++ controllers/controller.js
@@ -23,35 +23,53 @@ const controller = {
         },
 
         getQueryResults: async function (req, res) {
-                var result = [];
-                var query = req.query.query_holder;
+            var result = [];
+            var query = req.query.query_holder.trim();
+            
+            if (query.split(" ")[0].toUpperCase () == 'SELECT') {
+                var queryChecker = query.split('FROM')[0].split(" ");
+                console.log("hello"+ queryChecker [1] + "hello");
+                
+                if (!queryChecker.includes("*\n") && !queryChecker.includes("*")) {
+                    queryChecker = queryChecker.join(',').split(',');
+                    var checker1 = queryChecker.includes("id");
+                    var checker2 = queryChecker.includes("ID");
+                    var checker3 = queryChecker.includes("Id");
+                    var checker4 = checker = queryChecker.includes("iD");
 
-                if (query.trim().split(" ")[0].toUpperCase () == 'SELECT') {
-                        try {
-                                result = await db.select_query(query);
-
-                                var uniqueKeys = result.reduce(function (acc, obj) {
-                                        return acc.concat(Object.keys(obj).filter(key => acc.indexOf(key) === -1));
-                                }, []);  
-                                
-                                resultlen = result.length;
-                                result = result.slice(0, 50);
-                                
-                                var data = {
-                                        uniqueKeys: uniqueKeys,
-                                        result: result,
-                                        resultlen: resultlen
-                                };
-                                res.render('home', data);
-                        } catch (err) {
-                                console.log ("Error in the given MySQL query.");
-                                res.render('home', err);
-                        }                        
-                } else {
-                        console.log ('Query not allowed.');
-                        var error = "Only SELECT queries can be executed."
-                        res.render('home', error);
+                    if (!(checker1 || checker2 || checker3 || checker4)) {
+                        query = query.substring(0, 6) + " id, " + query.substring(6, query.length); 
+                    } 
                 }
+
+                console.log (query);
+
+                try {
+                        result = await db.select_query(query);
+
+                        var uniqueKeys = result.reduce(function (acc, obj) {
+                                return acc.concat(Object.keys(obj).filter(key => acc.indexOf(key) === -1));
+                        }, []);  
+                        
+                        resultlen = result.length;
+                        result = result.slice(0, 50);
+                            
+                        var data = {
+                                uniqueKeys: uniqueKeys,
+                                result: result,
+                                resultlen: resultlen
+                        };
+                        
+                        res.render('home', data);
+                } catch (err) {
+                        console.log ("Error in the given MySQL query.");
+                        res.render('home', err);
+                }                        
+            } else {
+                    console.log ('Query not allowed.');
+                    var error = "Only SELECT queries can be executed."
+                    res.render('home', error);
+            }
         },
 
         updateMovie: async function (req, res) {
