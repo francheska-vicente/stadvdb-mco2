5443c5efa63af218a1a34ced650ee4d99b9bdd08
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2022-02-13 17:34:03 +0800

    Update sync.js

--- models/sync.js
+++ models/sync.js
@@ -9,8 +9,8 @@ const sync_funcs = {
     sync_leader_node: async function () {
         let logs = [];
         try {
-            var logs2 = await transaction.make_transaction(2, queryHelper.to_retrieve_logs(1), 'SELECT');
-            var logs3 = await transaction.make_transaction(3, queryHelper.to_retrieve_logs(1), 'SELECT');
+            var logs2 = await transaction.make_transaction(2, queryHelper.to_retrieve_logs(1), 'SELECT', '');
+            var logs3 = await transaction.make_transaction(3, queryHelper.to_retrieve_logs(1), 'SELECT', '');
             logs = logs2[0][0].concat(logs3[0][0]);
             logs.sort((a, b) => a.date.getTime() - b.date.getTime());
 
@@ -22,21 +22,21 @@ const sync_funcs = {
                         id = id[0][0].AUTO_INCREMENT;
                         query = queryHelper.to_insert_query(logs[i].name, logs[i].year, logs[i].rank); 
                         var log = queryHelper.to_update_query_log(id, logs[i].name, logs[i].year, logs[i].rank, logs[i].node_from, 1); 
-                        await transaction.make_transaction_with_log(logs[i].node_to, query, log, 'INSERT');
+                        await transaction.make_transaction_with_log(logs[i].node_to, query, log, 'INSERT', '');
                         break;
 
                     case 'UPDATE':
                         query = queryHelper.to_update_query(logs[i].id, logs[i].name, logs[i].rank, logs[i].year); 
-                        await transaction.make_transaction(logs[i].node_to, query, 'UPDATE');
+                        await transaction.make_transaction(logs[i].node_to, query, 'UPDATE', id);
                         break;
 
                     case 'DELETE':
                         query = queryHelper.to_insert_query(logs[i].id); 
-                        await transaction.make_transaction(logs[i].node_to, query, 'DELETE');
+                        await transaction.make_transaction(logs[i].node_to, query, 'DELETE', id);
                         break;
                 }
                 
-                return await transaction.make_transaction(logs[i].node_from, queryHelper.to_finish_log(logs[i].statement_id), 'UPDATE');
+                return await transaction.make_transaction(logs[i].node_from, queryHelper.to_finish_log(logs[i].statement_id), '', '');
             }
         }
         catch (error) {
@@ -63,7 +63,7 @@ const sync_funcs = {
                 }
                 console.log(query);
                 await transaction.make_transaction(logs[i].node_to, query);
-                return await transaction.make_transaction(logs[i].node_from, queryHelper.to_finish_log(logs[i].statement_id));
+                return await transaction.make_transaction(logs[i].node_from, queryHelper.to_finish_log(logs[i].statement_id), '', '');
             }
         }
         catch (error) {
