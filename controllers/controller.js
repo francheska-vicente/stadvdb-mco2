aec30b22355a0e53c2881e03d82fba62a43c7a66
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2022-02-15 01:00:37 +0800

    Update controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,22 +2,31 @@ const db = require('../models/db.js');
 
 const controller = {
         getIndex: async function (req, res) {
+                let pageNumber = parseInt(req.query.page) ? parseInt(req.query.page) : 1;
                 var result = [];
                 var query = `SELECT * FROM movies;`
                 result = await db.select_query(query);
                 var uniqueKeys = result.reduce(function (acc, obj) {
                         return acc.concat(Object.keys(obj).filter(key => acc.indexOf(key) === -1));
                 }, []);
+
                 result.sort((a, b) => a.id - b.id);
                 resultlen = result.length;
                 result = result.slice(0, 50);
+                result = result.slice(0 + (pageNumber - 1) * 100, (pageNumber - 1) * 100 + 100);
+
                 var data = {
                         uniqueKeys: uniqueKeys,
                         result: result,
-                        resultlen: resultlen
+                        resultlen: resultlen,
+                        pageNumberCurr: pageNumber,
+                        pageNumberPrev: pageNumber - 1,
+                        pageNumberNext: pageNumber + 1
                 };
+
                 res.render('home', data);
         },
+
 
         getQueryResults: async function (req, res) {
                 let pageNumber = parseInt(req.query.page) ? parseInt(req.query.page) : 1;
