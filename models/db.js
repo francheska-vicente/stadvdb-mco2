a3b4234df310e596bd039471e20da3ab825c9997
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2022-02-15 01:31:26 +0800

    revised crud

--- models/db.js
+++ models/db.js
@@ -42,10 +42,8 @@ const db_functions = {
         var query = queryHelper.to_insert_query(name, rank, year);
         var log;
 
-        try {
-            // if central node is up, insert row to central node and insert log based on year
-            await nodes.ping_node(1);
-
+        // if central node is up, insert row to central node and insert log based on year
+        if (ping_node(1)) {
             if (year < 1980)
                 log = queryHelper.to_insert_query_log(name, year, rank, 2, 1);
             else
@@ -54,10 +52,9 @@ const db_functions = {
             var result = transaction.make_transaction_with_log(1, query, log, 'INSERT', '');
             return (result instanceof Error) ? false : true;
         }
-        catch (error) {
-            // if central node is down, insert row to follower node based on year and insert log to central
-            console.log(error)
 
+        // if central node is down, insert row to follower node based on year and insert log to central
+        else {
             if (year < 1980) {
                 log = queryHelper.to_insert_query_log(name, year, rank, 1, 2);
                 var result = transaction.make_transaction_with_log(2, query, log, 'INSERT', '');
@@ -76,10 +73,8 @@ const db_functions = {
         var query = queryHelper.to_update_query(id, name, rank, new_year);
         var log, log2;
 
-        try {
-            // if central node is up, insert row to central node and insert log based on year
-            await nodes.ping_node(1);
-
+        // if central node is up, insert row to central node and insert log based on year
+        if (ping_node(1)) {
             // from 2, to 3
             if (new_year >= 1980 && old_year < 1980) {
                 log = queryHelper.to_delete_query_log(id, 2, 1);
@@ -104,12 +99,10 @@ const db_functions = {
                 var result = transaction.make_transaction_with_log(1, query, log, 'UPDATE', id);
                 return (result instanceof Error) ? false : true;
             }
-
         }
-        catch (error) {
-            // if central node is down, insert row to follower node based on year
-            console.log(error)
 
+        // if central node is down, update row in follower node based on year and insert log to central
+        else {
             // from 2, to 3
             if (new_year >= 1980 && old_year < 1980) {
                 query = queryHelper.to_delete_query(id);
@@ -139,7 +132,6 @@ const db_functions = {
                     return (result instanceof Error) ? false : true;
                 }
             }
-
         }
     },
 
@@ -148,10 +140,8 @@ const db_functions = {
         var query = queryHelper.to_delete_query(id);
         var log;
 
-        try {
-            // if central node is up, insert row to central node and insert log based on year
-            await nodes.ping_node(1);
-
+        // if central node is up, delete row from central node and insert log based on year
+        if (ping_node(1)) {
             if (year < 1980)
                 log = queryHelper.to_delete_query_log(id, 2, 1);
             else
@@ -160,10 +150,8 @@ const db_functions = {
             var result = transaction.make_transaction_with_log(1, query, log, 'DELETE', id);
             return (result instanceof Error) ? false : true;
         }
-        catch (error) {
-            // if central node is down, insert row to follower node based on year
-            console.log(error)
-
+        else {
+            // if central node is down, delete row from follower node based on year
             if (year < 1980) {
                 log = queryHelper.to_delete_query_log(id, 1, 2);
                 var result = transaction.make_transaction_with_log(2, query, log, 'DELETE', id);
