9d8d362e5471dfa84fdefb2245c7838b7a80e719
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2022-02-15 19:18:54 +0800

    Revert "locks"
    This reverts commit 8b44ad07e3eec7e63b6c501e093d76b46f08c341.

--- models/transaction.js
+++ models/transaction.js
@@ -45,15 +45,12 @@ const transactions_funcs = {
         }
     },
 
-    insert_update_transaction: async function (node_to, query, update, node_from, type, id) {
+    insert_update_transaction: async function (node_to, query, update, node_from) {
         try {
             let conn = await nodes.connect_node(node_to);
             if (conn)
                 try {
                     await conn.beginTransaction();
-
-                    if (type === 'UPDATE' || type === 'DELETE')
-                        await conn.query(queryHelper.to_select_for_update(id));
 
                     await conn.query(`SET @@session.time_zone = "+08:00";`);
                     var result = await conn.query(query);
